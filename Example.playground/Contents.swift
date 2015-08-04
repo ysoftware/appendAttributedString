12883371822a5d0016f61088b5c46a4a98853e25
ysoftware <ysoftware@yandex.ru>
2015-08-04 14:28:24 +0300

    Live Example

--- Example.playground/Contents.swift
+++ Example.playground/Contents.swift
@@ -11,5 +11,6 @@ string.appendHeadline("BOOKS!\n")
 string.append("Books are the best weapon in the world.\n", withColor: .brownColor(), andStyle: UIFontTextStyleHeadline)
 string.append("This room's the greatest arsenal we could have!\n", withColor: .orangeColor(), andFont: UIFont(name: "Georgia", size: 20)!)
 string.append("Arm yourself\n\n", withColor: UIColor.grayColor(), style: UIFontTextStyleHeadline, andUnderlineStyle: NSUnderlineStyle.StyleThick)
+
 string.appendHeadline("© The Doctor\n")
 string.append("Quote by Russel T Davies", withColor: .purpleColor(), font: UIFont(name: "Georgia", size: 15)!, andStrikeThroughStyle: NSUnderlineStyle.StyleSingle)
