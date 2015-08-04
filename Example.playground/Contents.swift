d0eec673d72211afa25f750a2c273100dde30556
ysoftware <ysoftware@yandex.ru>
2015-08-04 14:07:16 +0300

    Added colors, underlines and strike through lines

---
+++ Example.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Library - noun: a place where people can learn
+
+import UIKit
+
+let view = UIView(frame: CGRectMake(0, 0, 450, 300))
+let textView = UITextView(frame: view.bounds)
+view.addSubview(textView)
+
+
+var string = NSMutableAttributedString()
+
+string.appendSubheadline("You want weapons?\n")
+string.appendBody("We're in a library.\n")
+string.appendHeadline("BOOKS!\n")
+
+string.append("Books are the best weapon in the world.\n", withColor: .brownColor(), andStyle: UIFontTextStyleHeadline)
+string.append("This room's the greatest arsenal we could have!\n", withColor: .orangeColor(), andFont: UIFont(name: "Georgia", size: 20)!)
+string.append("Arm yourself\n\n", withColor: UIColor.grayColor(), style: UIFontTextStyleHeadline, andUnderlineStyle: NSUnderlineStyle.StyleThick)
+string.appendHeadline("© The Doctor\n")
+string.append("Quote by Russel T Davies", withColor: .purpleColor(), font: UIFont(name: "Georgia", size: 15)!, andStrikeThroughStyle: NSUnderlineStyle.StyleSingle)
+
+textView.attributedText = string
+view
