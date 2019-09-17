321118b33541ec768d3cbe1b5460adfc7a6892b2
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-09-17 20:18:41 +0300

    adding package manager

---
+++ appendAttributedString/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "appendAttributedStringWithEase",
+    products: [
+        .library(
+            name: "appendAttributedStringWithEase",
+            targets: ["appendAttributedStringWithEase"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "appendAttributedStringWithEase",
+            dependencies: []),
+        .testTarget(
+            name: "appendAttributedStringWithEase",
+            dependencies: ["appendAttributedStringWithEase"]),
+    ]
+)
